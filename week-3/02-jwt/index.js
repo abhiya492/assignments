13f664c39aa3c32cac49e13d35b328c7216b8b44
Abhishek Singh <117256099+abhiya492@users.noreply.github.com>
2024-10-04 12:57:14 +0000

    week-3 done

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,5 +1,9 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
+const zod=require("zod")
+
+const emailSchema=zod.string().email();
+const passwordSchema=zod.string().min(6);
 
 
 /**
@@ -14,6 +18,14 @@ const jwtPassword = 'secret';
  *                        the password does not meet the length requirement.
  */
 function signJwt(username, password) {
+    const usernameValidation=emailSchema.safeParse(username);
+    const passwordValidation=passwordSchema.safeParse(password);
+    if(usernameValidation.success==false || passwordValidation.success==false){
+        return null;
+    }
+    const signature =jwt.sign({
+        username},jwtPassword)
+        return signature;
     // Your code here
 }
 
@@ -26,7 +38,14 @@ function signJwt(username, password) {
  *                    using the secret key.
  */
 function verifyJwt(token) {
-    // Your code here
+    let ans=true;
+    try{
+        verified=jwt.verify(token,jwtPassword);
+    }
+    catch(error){
+        ans= false;
+    }
+    return ans;
 }
 
 /**
@@ -37,7 +56,12 @@ function verifyJwt(token) {
  *                         Returns false if the token is not a valid JWT format.
  */
 function decodeJwt(token) {
-    // Your code here
+    const decoded=jwt.decode(token);
+    if(decoded){
+        return true;
+    }{
+        return false;
+    }
 }
 
 
